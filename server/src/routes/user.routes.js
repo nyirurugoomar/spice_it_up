2434b66584a292042332cc93d05fcfefd940c523
Omar <nyirurugoomar@gmail.com>
2025-06-16 21:00:19 +0200

    Update base for backend url

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { signup, signin } = require('../controllers/user.controller');
 
+// Test endpoint
+router.get('/test', (req, res) => {
+  res.json({ message: 'Backend is running!' });
+});
+
 router.post('/signup', signup);
 router.post('/signin', signin);
 
