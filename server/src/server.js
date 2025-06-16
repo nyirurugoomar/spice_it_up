11b049d08914220db60a8e70bc19b195bb979dd8
Omar <nyirurugoomar@gmail.com>
2025-06-16 19:50:35 +0200

    Updating url for backend to origin

--- server/src/server.js
+++ server/src/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173','https://spiceitup-app.netlify.app/'],
+  origin: ['http://localhost:5173','https://spiceitup-app.netlify.app'],
   credentials: true,
 }));
 // Routes
